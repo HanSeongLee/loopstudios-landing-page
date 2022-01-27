a2685dc070eed4fdc0fb3a6bad4ae2fd31718503
HanSeong Lee <jkhanseong@gmail.com>
2022-01-27 19:35:50 +0900

    ADD | Intro section on desktop

--- components/IntroSection/index.js
+++ components/IntroSection/index.js
@@ -6,19 +6,30 @@ const IntroSection = () => {
     return (
         <section className={styles.introSection}>
             <Container className={styles.container}>
-                <img className={styles.image}
-                     src={'/img/mobile/image-interactive.jpg'}
-                     alt={'interactive'}
-                />
-                <h2 className={styles.title}>
-                    The leader in interactive VR
-                </h2>
-                <p className={styles.description}>
-                    Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of
-                    the
-                    best companies around the globe. Our award-winning creations have transformed businesses through
-                    digital experiences that bind to their brand.
-                </p>
+                <picture>
+                    <source srcSet={'/img/image-interactive.jpg'}
+                            media={'(min-width: 768px)'}
+                    />
+                    <img className={styles.image}
+                         src={'/img/mobile/image-interactive.jpg'}
+                         alt={'interactive'}
+                    />
+                </picture>
+                <div className={styles.contentWrapper}>
+                    <div className={styles.content}>
+                        <h2 className={styles.title}>
+                            The leader in interactive VR
+                        </h2>
+                        <p className={styles.description}>
+                            Founded in 2011, Loopstudios has been producing world-class virtual reality projects for
+                            some of
+                            the
+                            best companies around the globe. Our award-winning creations have transformed businesses
+                            through
+                            digital experiences that bind to their brand.
+                        </p>
+                    </div>
+                </div>
             </Container>
         </section>
     );
